df2aa028ae85a01de92ac168486d8b2db21715da
Devesh Saini <deveshsaini369@gmail.com>
2025-11-14 23:08:42 +0530

    Dependencies added, View added for home page.

---
+++ src/main/java/com/dev/store/HomeController.java
@@ -0,0 +1,14 @@
+package com.dev.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String index() {
+        String s = "index.html";
+        return s;
+    }
+}
